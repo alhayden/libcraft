63d22127af1f66adce02672027a0a11a5b0911ea
Dylan <dylanjones2011@gmail.com>
2020-05-12 20:29:07 -0400

    string ids

--- src/bin/libcraftd.rs
+++ src/bin/libcraftd.rs
@@ -10,17 +10,16 @@ use std::ffi::OsStr;
 use warp::Filter;
 
 
-
 #[tokio::main]
 async fn main() {
     let not_found = warp::any().map(|| "404.");
     let server_list = warp::path!("server").map(list);
-    let server_get = warp::path!("server" / i32).map(serialize_server);
+    let server_get = warp::path!("server" / String).map(serialize_server);
     let server_create = warp::path!("server").map(create);
-    let server_edit = warp::path!("server" / i32 / "edit").map(|frederick|"edit");
-    let server_delete = warp::path!("server" / i32 / "delete").map(|bill|"ded");
-    let server_start= warp::path!("server" / i32 / "start").map(start);
-    let server_stop = warp::path!("server" / i32 / "stop").map(stop);
+    let server_edit = warp::path!("server" / String / "edit").map(|frederick| "edit");
+    let server_delete = warp::path!("server" / String / "delete").map(|bill| "ded");
+    let server_start = warp::path!("server" / String / "start").map(start);
+    let server_stop = warp::path!("server" / String / "stop").map(stop);
 
     let server_map: Arc<Mutex<HashMap<String, Arc<Server>>>> = Arc::new(Mutex::new(HashMap::new()));
     load(server_map.clone());
@@ -36,19 +35,21 @@ async fn main() {
 fn list() -> &'static str {
     "lsit of servers"
 }
-fn serialize_server(id: i32) -> &'static str {
-    "here be text"
+
+fn serialize_server(id: String) -> String {
+    "here be text, the server id be ".to_owned() + &id
 }
 
 fn create() -> &'static str {
     "u just made a server congrats"
 }
 
-fn start(id: i32) -> &'static str {"go"
-
+fn start(id: String) -> &'static str {
+    "go"
 }
 
-fn stop(id: i32) -> &'static str {"stop"
+fn stop(id: String) -> &'static str {
+    "stop"
 }
 
 
@@ -60,7 +61,7 @@ struct Server {
     jarfile: String,
     pwd: String,
     jvm_args: String,
-    process: Option<Child>
+    process: Option<Child>,
 }
 
 impl Server {
@@ -71,39 +72,39 @@ impl Server {
         };
         let mut contents = String::new();
         match file.read_to_string(&mut contents) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(e) => return Err(e),
         };
 
-        let confs = match YamlLoader::load_from_str(&mut contents){
+        let confs = match YamlLoader::load_from_str(&mut contents) {
             Ok(cfg) => cfg,
             Err(e) => return Err(Error::new(ErrorKind::Other, e)),
         };
         let conf: &Yaml = &confs[0];
 
-        if ! check_yaml_correct(conf) {return Err(Error::new(ErrorKind::Other, "Malformed YAML"))} //somehow error out here
+        if !check_yaml_correct(conf) { return Err(Error::new(ErrorKind::Other, "Malformed YAML")); } //somehow error out here
 
         dbg!(conf);
         Ok(
-        Server {
-            //TODO BETTER ERROR HANDLING
-            yaml_path: yaml_path_arg,
-            commit: false,
-            //TODO VERIFY SAFETY
-            id: conf["id"].as_str().unwrap().to_string(),
-            name: conf["name"].as_str().unwrap().to_string(),
-            jarfile: conf["jarfile"].as_str().unwrap().to_string(),
-            pwd: conf["pwd"].as_str().unwrap().to_string(),
-            jvm_args: conf["jvm-args"].as_str().unwrap().to_string(),
-            process: None
-        })
+            Server {
+                //TODO BETTER ERROR HANDLING
+                yaml_path: yaml_path_arg,
+                commit: false,
+                //TODO VERIFY SAFETY
+                id: conf["id"].as_str().unwrap().to_string(),
+                name: conf["name"].as_str().unwrap().to_string(),
+                jarfile: conf["jarfile"].as_str().unwrap().to_string(),
+                pwd: conf["pwd"].as_str().unwrap().to_string(),
+                jvm_args: conf["jvm-args"].as_str().unwrap().to_string(),
+                process: None,
+            })
     }
 
-    fn start(&mut self)  -> String {
+    fn start(&mut self) -> String {
         dbg!(&self.process);
         match &self.process {
             None => return String::from("Could not start process: server is already running"),
-            Some(c) => {},
+            Some(c) => {}
         };
         let mut proc = process::Command::new("java");
         for arg in self.jvm_args.split(" ") {
@@ -114,7 +115,7 @@ impl Server {
         proc.current_dir(&self.pwd);
         match proc.spawn() {
             Ok(childProc) => self.process = Some(childProc),
-            Err(e) => {return String::from("Failed to start process: error in spawn");},
+            Err(e) => { return String::from("Failed to start process: error in spawn"); }
         };
         let s: String = "aaa".parse().unwrap();
         println!("{}", s);
@@ -126,12 +127,12 @@ impl Server {
 fn load(server_map: Arc<Mutex<HashMap<String, Arc<Server>>>>) {
     println!("Attempting to load server yaml files...");
     for entry in read_dir(Path::new(".")).unwrap() {//TODO THIS IS NOT THE FINAL PATH
-            let entry = entry.unwrap();
-            let path = entry.path();
-            if !path.is_dir() && path.extension().unwrap_or(OsStr::new("urbad")) == OsStr::new("yaml") {
-                load_server(server_map.clone(), String::from(path.to_str().unwrap()));
-            }
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if !path.is_dir() && path.extension().unwrap_or(OsStr::new("urbad")) == OsStr::new("yaml") {
+            load_server(server_map.clone(), String::from(path.to_str().unwrap()));
         }
+    }
     println!("Done!  {} servers loaded", server_map.lock().unwrap().len());
 }
 
@@ -141,8 +142,8 @@ fn load_server(server_map: Arc<Mutex<HashMap<String, Arc<Server>>>>, filename: S
         Ok(srv) => {
             server_map.lock().unwrap().insert(String::from(&(srv.name)), Arc::new(srv));
             println!("Successfully loaded server!");
-        },
-        Err(e) => {println!("Failed to load server: {}",e)}
+        }
+        Err(e) => { println!("Failed to load server: {}", e) }
     }
 }
 
